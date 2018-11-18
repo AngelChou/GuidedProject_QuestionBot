4e8ded05abd643162747d2322477d7e6a679280e
Angel Chou <cat77120377@hotmail.com>
2018-11-18 16:43:17 +0800

    update question

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -12,6 +12,14 @@ struct MyQuestionAnswerer {
             return "Yes, you can!"
         } else if lowerQuestion.hasPrefix("should"){
             return "No, you shouldn't!"
+        } else if lowerQuestion == "why you call penguin"{
+            return "Because I look like a penguin."
+        } else if lowerQuestion == "who am i" {
+            return "I'm SpiderMan!"
+        } else if lowerQuestion == "what is your favorite movie?" {
+            return "Mission Impossible!"
+        } else if lowerQuestion.hasPrefix("我會") && lowerQuestion.hasSuffix("嗎"){
+            return "只要你想要，沒有什麼事是不可能的！"
         } else {
             return "?"
         }
